cea17abf7767d2eda130f37cb0860d7feaf24ff1
kelley <kelleylsharp@gmail.com>
2018-12-05 18:38:21 -0800

    onward

--- api-classes.js
+++ api-classes.js
@@ -7,11 +7,29 @@ class StoryList {
     $.get('https://hack-or-snooze-v2.herokuapp.com/stories', function(
       response
     ) {
-      var storyList = new StoryList(
+      let storyList = new StoryList(
         response.stories.map(story => new Story(story))
       );
       cb(storyList);
     });
+  }
+
+  static addStory(currentUser, storyObj, cb){
+    $.post('https://hack-or-snooze-v2.herokuapp.com/stories', function(response){
+      let newStory = new Story(storyObj);
+      newStory.setToken('token', response.token)
+      newStory.author = response.author;
+      newStory.title = response.title;
+      newStory.url = response.url;
+
+      //but the arg is a cb?
+      let user = this.retrieveDetails(currentUser);
+      user.stories = user.stories.unshift(newStory);
+
+      // currentUser.retrieveDetails(function(user){
+      //   user.stories.unshift(newStory);
+      // });
+    })
   }
 }
 
@@ -28,7 +46,7 @@ class User {
     this.loginToken = token;
     localStorage.setItem('token', token);
   }
- 
+
   static create(username, password, name, cb) {
     $.post(
       'https://hack-or-snooze-v2.herokuapp.com/users',
@@ -39,6 +57,28 @@ class User {
         cb(newUser);
       }
     );
+  }
+
+  static login(username, password, cb) {
+    $.post(
+      'https://hack-or-snooze-v2.herokuapp.com/login',
+      { user: { username, password } },
+      function(response) {
+        let loggedInUser = new User();
+        loggedInUser.setToken(response.token);
+        cb(loggedInUser);
+      }
+    );
+  }
+
+  static retrieveDetails(cb) {
+    $.get('https://hack-or-snooze-v2.herokuapp.com/users', function(response) {
+      let user = new User();
+      user.setToken(response.token);
+      user.stories = response.stories;
+      user.favorites = response.favorites;
+      cb(user);
+    });
   }
 }
 
