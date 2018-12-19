a83cb1a28be12a1bcce3bc1c7b7c7040e11b7c3f
kelley <kelleylsharp@gmail.com>
2018-12-18 20:59:08 -0800

    stories appear

--- js/StoryList.js
+++ js/StoryList.js
@@ -1,32 +1,33 @@
+const API_URL = 'https://hack-or-snooze-v2.herokuapp.com';
+
 class StoryList {
   constructor(stories = []) {
     this.stories = stories;
   }
 
-  static getStories(cb) {
-    $.get('https://hack-or-snooze-v2.herokuapp.com/stories', function(
-      response
-    ) {
+  static getStories(done) {
+    $.get(`${API_URL}/stories`, function processResponse(response) {
       let storyList = new StoryList(
         response.stories.map(story => new Story(story))
       );
-      cb(storyList);
+      done(storyList);
     });
   }
 
-  static addStory(currentUser, storyObj, cb){
-    $.post('https://hack-or-snooze-v2.herokuapp.com/stories', function(response){
+  static addStory(currentUser, storyObj, theStoryIsReadyNow) {
+    $.post(`${API_URL}/stories`, function(response) {
       let newStory = new Story(storyObj);
       newStory.setToken('token', response.token);
       newStory.author = response.author;
       newStory.title = response.title;
       newStory.url = response.url;
-      newStory.preview = response.preview;
+
+      theStoryIsReadyNow(newStory);
 
       //but the arg is a cb?
       let user = this.retrieveDetails(currentUser);
       user.stories = user.stories.unshift(newStory);
 
-    })
+    });
   }
 }
--- js/index.js
+++ js/index.js
@@ -1,38 +1,30 @@
-$( document ).ready(function() {
+$(document).ready(function() {
   generateStories();
-
 });
 
-
-  
 let LOGGED_IN = false;
 
 let user;
 
-
-
-function generateStoryHTML(story){
-  const storyMarkup = 
-  "<p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray story-preview">
+function generateStoryHTML(story) {
+  const storyMarkup = `<p class="media-body pb-3 pt-3 mb-0 small lh-125 border-bottom border-gray story-preview">
     <strong class="d-block text-gray-dark title">
       <i class="far fa-star"></i>
       ${story.title}
     </strong>
-    <small>${story.author}</small>
-    <small>${story.preview}</small>
-  </p>"
-  $(".story-list-area").prepend(storyMarkup);
+    <small>posted by: ${story.author}</small>
+  </p>`;
+  $('.story-list-area').append(storyMarkup);
 }
 
 //to get the list of stories
-function generateStories(){
-  let currentStories = StoryList.getStories();
-  currentStories.stories.map(story => {
-    generateStoryHTML(story);
+function generateStories() {
+  StoryList.getStories(function handleResponse(currentStories) {
+    currentStories.stories.forEach(story => {
+      generateStoryHTML(story);
+    });
   });
 }
-
-
 
 //when user clicks signup/login, render the correct form/html.
 
@@ -44,9 +36,7 @@ $('#login-submit').on('click', function(e) {
     event.preventDefault();
     User.login(username, password, function afterYouLoggedIn(theUser) {
       // you're finally logged in
+    });
   });
-})
   LOGGED_IN = true;
 });
-
-
